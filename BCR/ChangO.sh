39de0f8a7492f4d08af410c5119b70728d8a934e
Bei-lab <52922045+bei-lab@users.noreply.github.com>
2022-06-23 18:41:52 +0800

    Update ChangO.sh

--- BCR/ChangO.sh
+++ BCR/ChangO.sh
@@ -1,8 +1,8 @@
-AssignGenes.py igblast -s npc_merge_bcr.fasta -b /data/home/liuyang/software/ncbi-igblast-1.17.0 \
+AssignGenes.py igblast -s npc_merge_bcr.fasta -b /software/ncbi-igblast-1.17.0 \
 --organism human --loci ig --format blast
 
 MakeDb.py igblast -i npc_merge_bcr_igblast.fmt7 -s npc_merge_bcr.fasta \
--r /data/home/liuyang/liuyang/RAW_data/1-SingleCell/3-NPC/analysis/NPC-BCR/Change-O/database/AIRR_Example/IMGT_Human_*.fasta \
+-r /NPC/analysis/NPC-BCR/Change-O/database/AIRR_Example/IMGT_Human_*.fasta \
 --10x bcr_merge_10x.csv --extended
 
 ParseDb.py select -d npc_merge_bcr_igblast_db-pass.tsv -f productive -u T
